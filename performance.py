f7055bebedec76e88cc6d09e094f5252aa44cdab
MR-NBD <145067193+MR-NBD@users.noreply.github.com>
2024-05-19 00:25:49 +0200

    Update performance.py
    FINAL SCRIPT

--- performance.py
+++ performance.py
@@ -1,25 +1,38 @@
-import iperf3
 import subprocess
 import argparse
 from colorama import Fore, Style
+import re
+import math
+from datetime import datetime
 
 
-def perf(client, options, receive, name, protocol):
+def perf(options, protocol, receive):
+    # Esegui il comando iperf3 e cattura l'output
+    result = subprocess.run(
+        ["iperf3", "-c", options.address, protocol, receive],
+        capture_output=True,
+        text=True,
+    )
+
+    # Controlla se il comando è stato eseguito con successo
+    if result.returncode == 0:
+        output = result.stdout
+
+        # Utilizza regex per trovare i Bitrate del sender e del receiver
+        sender_bitrate = re.search(
+            r"\[.*\]\s+0.00-10.00\s+sec\s+.*\s+(\d+\.\d)+ Gbits/sec", output
+        )
+
+        return float(sender_bitrate.group(1))
+    else:
+        print(
+            Fore.RED
+            + f"[-] Error executing the iperf3 command. Error message: {result.stderr}"
+        )
+
+
+def dump(options, protocol, receive, file):
     # Set vars
-    remote_site = options.address
-    test_duration = 10
-
-    # Set Iperf Client Options
-    client.server_hostname = remote_site
-    client.zerocopy = True
-    client.verbose = False
-    client.port = 5201
-    client.num_streams = 10
-    client.duration = int(test_duration)
-    client.protocol = protocol  # PROBLEMA
-    client.reverse = receive
-    client.length = 1472
-    client.bandwidth = 50000000000
 
     try:
         dump = subprocess.Popen(
@@ -29,7 +42,7 @@ def perf(client, options, receive, name, protocol):
                 "-i",
                 options.interface,
                 "-w",
-                name + "-" + options.filename,
+                options.filename + file,
             ],
             stdin=subprocess.PIPE,
         )
@@ -37,24 +50,30 @@ def perf(client, options, receive, name, protocol):
         dump.stdin.flush()
 
         # Run iperf3 test
-        result = client.run()
 
+        result = []
         # extract relevant data
-        sent_mbps = int(result.sent_Mbps)
-        received_mbps = int(result.received_Mbps)
+        for i in range(10):
+            result.append(perf(options, protocol, receive))
+            print(Fore.GREEN + f"[{i}] Sender Bitrate: {result[-1]}" + Style.RESET_ALL)
 
-        print(Fore.YELLOW + f"{protocol.upper()} sent_mbps: {sent_mbps}")
-        print(Fore.YELLOW + f"{protocol.upper()} received_mbps: {received_mbps}")
-        print(Fore.YELLOW + f"{protocol.upper()} reverse mode : {receive}")
-        print(Style.RESET_ALL)
+        Min = min(result)  # MIN
+        Max = max(result)  # MAX
+        mean_value = sum(result) / len(result)  # MEAN
+        sum_squared_diff = sum((x - mean_value) ** 2 for x in result)
+        std_dev_value = math.sqrt(sum_squared_diff / (len(result) - 1))
+        return Min, Max, mean_value, std_dev_value
 
     except Exception as e:
-        print(Fore.RED + f"Error during UDP test: {e}")
-        print(Style.RESET_ALL)
+        print(Fore.RED + f"[-] Error during Dumping: {e}" + Style.RESET_ALL)
+
     finally:
         dump.terminate()
-        print(Fore.GREEN + f"[+] dump made for {options.filename} {name}")
-        print(Style.RESET_ALL)
+        print(
+            Fore.GREEN
+            + f"[+] --> DUMP Made Correctly for the file : {options.filename}{file}"
+            + Style.RESET_ALL
+        )
 
 
 def main():
@@ -66,16 +85,68 @@ def main():
     parser.add_argument("-a", "--address", dest="address", help="write the address")
 
     options = parser.parse_args()
+    # Calcola la data e l'ora corrente
+    current_datetime = datetime.now()
+    formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
 
-    client = iperf3.Client()
-    perf(client, options, receive=False, name="TCP", protocol="tcp")
+    print(Fore.BLUE + f"[+] TCP test" + Style.RESET_ALL)
+    Min_TCP, Max_TCP, avg_TCP, std_dev_TCP = dump(
+        options, protocol="", receive="-R", file="-TCP"
+    )
+    print(Fore.BLUE + f"[+] UDP test" + Style.RESET_ALL)
+    Min_UDP, Max_UDP, avg_UDP, std_dev_UDP = dump(
+        options, protocol="-i 1 -u -b 50M", receive="", file="-UDP"
+    )
+    print(Fore.BLUE + f"[+] TCP test with receive instead of sending" + Style.RESET_ALL)
+    Min_TCP_R, Max_TCP_R, avg_TCP_R, std_dev_TCP_R = dump(
+        options, protocol="", receive="-R", file="-TCP-R"
+    )
+    print(Fore.BLUE + f"[+] UDP with receive instead of sending" + Style.RESET_ALL)
+    Min_UDP_R, Max_UDP_R, avg_UDP_R, std_dev_UDP_R = dump(
+        options, protocol="-i 1 -u -b 50M", receive="-R", file="-UDP-R"
+    )
 
-    clientB = iperf3.Client()
-    perf(clientB, options, receive=False, name="UDP", protocol="udp")
-    clientC = iperf3.Client()
-    perf(clientC, options, receive=True, name="TCP-REC", protocol="tcp")
-    clientD = iperf3.Client()
-    perf(clientD, options, receive=True, name="UDP-REC", protocol="udp")
+    print(Fore.BLUE + f"|----------- FINAL RESULT -----------|")
+    print(Fore.BLUE + f"|       | Avg  | Min  | Max  | StdD  |")
+    print(
+        Fore.BLUE
+        + f"|  TCP  | {avg_TCP:.1f} | {Min_TCP:.1f} | {Max_TCP:.1f} | {std_dev_TCP:.1f}   |"
+    )
+    print(
+        Fore.BLUE
+        + f"|  UDP  | {avg_UDP:.1f} | {Min_UDP:.1f} | {Max_UDP:.1f} | {std_dev_UDP:.1f}   |"
+    )
+    print(
+        Fore.BLUE
+        + f"| TCP_R | {avg_TCP_R:.1f} | {Min_TCP_R:.1f} | {Max_TCP_R:.1f} | {std_dev_TCP_R:.1f}   |"
+    )
+    print(
+        Fore.BLUE
+        + f"| UDP_R | {avg_UDP_R:.1f} | {Min_UDP_R:.1f} | {Max_UDP_R:.1f} | {std_dev_UDP_R:.1f}   |"
+    )
+    print(Style.RESET_ALL)
+
+    with open(f"RESULT-{options.filename}.txt", "w") as file:
+        # Scrivi la data e l'ora corrente come prima riga
+        file.write(formatted_datetime + "\n")
+
+        # Scrivi il contenuto richiesto
+        file.write(f"|----------- FINAL RESULT -----------|\n")
+        file.write(f"|       | Avg  | Min  | Max  | StdD  |\n")
+        file.write(
+            f"|  TCP  | {avg_TCP:.1f} | {Min_TCP:.1f} | {Max_TCP:.1f} | {std_dev_TCP:.1f}   |\n"
+        )
+        file.write(
+            f"|  UDP  | {avg_UDP:.1f} | {Min_UDP:.1f} | {Max_UDP:.1f} | {std_dev_UDP:.1f}   |\n"
+        )
+        file.write(
+            f"| TCP_R | {avg_TCP_R:.1f} | {Min_TCP_R:.1f} | {Max_TCP_R:.1f} | {std_dev_TCP_R:.1f}   |\n"
+        )
+        file.write(
+            f"| UDP_R | {avg_UDP_R:.1f} | {Min_UDP_R:.1f} | {Max_UDP_R:.1f} | {std_dev_UDP_R:.1f}   |\n"
+        )
+
+    print(Fore.GREEN + f"[+] --> RESULT SAVE ON : {options.filename}" + Style.RESET_ALL)
 
 
 if __name__ == "__main__":
